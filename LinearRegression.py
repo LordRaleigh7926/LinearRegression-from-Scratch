33a7fef2ca22f60153386f0daa97e98282596d29
LordRaleigh7926 <ankushroy7926@outlook.com>
2023-12-17 21:11:29 +0530

    First commit

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,42 +1,54 @@
 #A class for Loss functions
 class LossFunction:
 
-    #Init function. Takes in number . Takes in num of rows
-    def __init__(self, method='MSE', m=2):
-        self.m = m #m = number of rows || number of datapoints
-
     # Mean Squared Error. One of the loss functions
     def Mean_Squared_Error(self, pred, actual): 
         
         #Takes in self. list of Predicted values (ŷ). List of Actual values (y) 
 
-        loss = 0 # MSE = 1/(2m)* Σ((ŷ-y)^2), iterated m times
+        # m stores number of DataPoints
+        m = len(pred) 
 
-        for i in range(self.m):
+        # MSE = 1/(2m)* Σ((ŷ-y)^2), iterated m times. For more info search MSE formula
+        loss = 0 
+
+        # Iterating m times
+        for i in range(m): 
             loss = loss+(actual[i]-pred[i])**2
 
-        FinalLoss = loss/(self.m)
+        FinalLoss = loss/(2*m)
+
         return FinalLoss
 
 
-        
-
+''' 
+Gradient Descent Function.
+Used to minimize error and get to the best weights and bias.
+Takes in x, y and the learning rate and the number of epochs
+Formula for Gradient Descent for Linear Regression
+'''
 def Gradient_Descent(x, y, alpha=0.0001, epochs=300):
 
+    # m stores number of DataPoints
     m = len(x)
 
+    #Setting the main Bias and Weight to 0
     weights = 0
     bias= 0
 
+    # For loop for epochs
     for _ in range(epochs):
 
+        # Setting the updated weight and updated bias to 0 after every iteration
         updated_weights = updated_bias = 0
 
+        # Loop for adding the derivated error for all the datapoints
         for i in range(m):
         
             updated_weights += -(2/m)*x[i]*(y[i]-(weights*x[i]+bias))
             updated_bias += -(2/m)*(y[i]-(weights*x[i]+bias))
 
+        #Updating the weights and biases
         weights = weights - updated_weights*alpha
         bias = bias - updated_bias*alpha
 
@@ -54,24 +66,31 @@ class LinearRegressor:
         self.J = loss_function
         self.bias = self.weights = None
 
+    # Used to train the model and set the weights and bias
+    # It uses the function Gradient_Descent
     def train(self, x, y):
 
         self.weights, self.bias = Gradient_Descent(x, y, self.alpha)
         return self.weights,self.bias
 
+    # Used to predict a single y value for x
+    # Used in predict function
     def gettingValues(self, x):
         y = self.weights*x + self.bias
         return y
 
+    # This is of course used to predict the set of x values
+    # As mentioned earlier it uses the function gettingValues 
     def predict(self, x):
 
+        # if the model is not trained then it will throw an NotImplemented error
         if self.weights == None:
             raise  NotImplementedError("Model not Trained. Use model.train() to train it")
         
         PREDICTED_Value = []
 
+        # Loop for getting all the predicted value individually
         for i in x:
-            print(i)
             PREDICTED_Value.append(self.gettingValues(i))
 
         return PREDICTED_Value
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import numpy as np 
-from functions import LinearRegressor
+from LinearRegression import LinearRegressor
 import matplotlib.pyplot as plt
         
 a = LinearRegressor()
+
+# Using a Made Up Linear Data
 X_train = [1, 2, 45, 65, 77, 95]
 y_train = [1, 2, 45, 65, 77, 95]
 
-plt.scatter(X_train,y_train)
 
 w, b= a.train(X_train,y_train)
 print(w,b)
@@ -15,15 +15,15 @@ y = a.predict([23,45,67,86,32,45])
 
 print(y)
 
-# Create the plot
-plt.plot([23,45,67,86,32,45], np.reshape(y,(6,)))
+# The correct Y Values
+plt.scatter([23,45,67,86,32,45],[23,45,67,86,32,45])
 
-# Add labels and title
+# Plotting our predicted values
+plt.plot([23,45,67,86,32,45], y)
+
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.title(f"Line with weight = {w} and bias = {b}")
-
-# Show the plot
 plt.show()
 
 
